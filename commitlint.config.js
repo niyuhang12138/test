ebbfb5375387a3707cb2139d965103e3a59c1c86
nyh <2830481694@qq.com>
2025-08-07 16:21:37 +0800

    :construction_worker: ci(auth): subject
    desc
    
    ISSUES CLOSED: #123

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,31 @@
 module.exports = {
-    // 自定义解析器：允许type包含emoji
-    extends: ['git-commit-emoji', 'cz']
-    // 自定义规则...
+    extends: ['cz'],
+    parserPreset: {
+        parserOpts: {
+            headerPattern:
+                /^(:[\\w+-]+:)\\s*(\\w+)(?:\\(([^)]*)\\))?:\\s*(.*)$/,
+            headerCorrespondence: ['emoji', 'type', 'scope', 'subject']
+        }
+    },
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'refactor',
+                'perf',
+                'test',
+                'build',
+                'ci',
+                'chore',
+                'revert',
+                'init'
+            ]
+        ],
+        'subject-case': [0] // 禁用主题大小写检查
+    }
 }
