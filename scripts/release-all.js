3ba2d2aef01feedff0c15ff5b3a08b6695617d28
nyh <2830481694@qq.com>
2025-08-08 12:49:56 +0800

    :sparkles: feat(packages/a): subject
    desc
    
    BREAKING CHANGE:
    yes
    
    ISSUES CLOSED: #4332

--- scripts/release-all.js
+++ scripts/release-all.js
@@ -11,3 +11,25 @@ matchedPackages.forEach((pkg) => {
 
 console.log('*********》 Releasing package：root《*********')
 execSync('semantic-release', { stdio: 'inherit', cwd: process.cwd() })
+
+// 获取最近一次 commit 的 scope
+function getLastCommitScope() {
+    try {
+        // 假设你的 commit 格式为 :emoji: type(scope): subject
+        const msg = execSync('git log -1 --pretty=%s', {
+            encoding: 'utf-8'
+        }).trim()
+        const match = msg.match(/^(:[\w_+-]+:)\s*\w+\(([^)]+)\):/)
+        if (match) {
+            return match[2]
+        }
+        return null
+    } catch (e) {
+        return null
+    }
+}
+
+if (getLastCommitScope() === 'root') {
+    console.log('*********》 Releasing package：root《*********')
+    execSync('semantic-release', { stdio: 'inherit', cwd: process.cwd() })
+}
